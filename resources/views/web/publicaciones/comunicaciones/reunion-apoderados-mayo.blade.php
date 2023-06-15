45f536c257b0db5aaf6a05cb2a0157aeb2185777
Emilio Correa <em.correa.soto@gmail.com>
2023-06-15 18:21:03 -0400

    Reunion de apoderados

---
+++ resources/views/web/publicaciones/comunicaciones/reunion-apoderados-mayo.blade.php
@@ -0,0 +1,22 @@
+@extends('web.template.template')
+
+@section('main')
+    <section id="team" class="team section-bg">
+        <div class="container">
+            <div class="section-title">
+                <h2 data-aos="fade-in">{{ $publicacion->nombre }}</h2>
+                <p data-aos="fade-in">Colegio Juan Pablo II</p>
+            </div>
+            <div class="row justify-content-center gy-4 mt-3">
+                <div class="col-10">
+                    <p>{{ $publicacionDetalle[0]->contenido }}</p>
+                </div>
+            </div>
+            <div class="row justify-content-center gy-4 mt-3">
+                <div class="col-10 text-center">
+                    <img src="{{ asset("archivos/reunion-apoderados-mayo/reunion-apoderados-mayo.jpeg") }}" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
